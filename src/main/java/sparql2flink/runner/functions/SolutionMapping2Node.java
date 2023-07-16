f6c6f28c912f74488c371f97d859a9216aa5ab9d
henrry <henrrymatituy@gmail.com>
2023-07-16 17:15:24 -0500

    Solucionado problema de compilación query

---
+++ src/main/java/sparql2flink/runner/functions/SolutionMapping2Node.java
@@ -0,0 +1,21 @@
+package sparql2flink.runner.functions;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.jena.graph.Node;
+
+//SolutionMapping to SolutionMapping - Map Function
+public class SolutionMapping2Node implements MapFunction<SolutionMapping, Node> {
+
+    private String label = null;
+
+    public SolutionMapping2Node(String label){
+        this.label= label;
+    }
+
+    @Override
+    public Node map(SolutionMapping sm){
+        System.out.println("Node: "+ sm.getMapping().get(label));
+        return sm.getMapping().get(label);
+    }
+}
+
