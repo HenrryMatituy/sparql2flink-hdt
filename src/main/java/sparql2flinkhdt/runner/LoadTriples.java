94d394ee6f67baaae49f03253583634c7d1d0f7b
henrry <henrrymatituy@gmail.com>
2023-12-12 10:09:47 -0500

    Funcionando hdt 3.0.10

--- src/main/java/sparql2flinkhdt/runner/LoadTriples.java
+++ src/main/java/sparql2flinkhdt/runner/LoadTriples.java
@@ -9,27 +9,41 @@ import org.rdfhdt.hdt.options.HDTSpecification;
 
 
 public class LoadTriples {
-	public static HDT fromDataset(ExecutionEnvironment environment, String filePath) {
-	    Preconditions.checkNotNull(filePath, "The file path may not be null...");
+//	public static HDT fromDataset(ExecutionEnvironment environment, String filePath) {
+//	    Preconditions.checkNotNull(filePath, "The file path may not be null...");
+//        HDT hdt = null;
+//
+//        try {
+//            String datasetFilePath = System.getProperty("user.dir") + "/examples/dataset.nt";
+//            System.out.println("Ruta completa del archivo: " + datasetFilePath);
+//
+//            String baseURI = "file://" + System.getProperty("user.dir") + "/examples/dataset.nt";
+//            hdt = HDTManager.generateHDT(filePath, "", RDFNotation.parse("ntriples"), new HDTSpecification(),null);
+//
+//        }catch (Exception e){
+//
+//            System.err.println("Error al generar el objeto HDT Henrry: " + e.getMessage());
+//            e.printStackTrace();  // Esto imprimirá la traza de la excepción
+//        }
+//        return hdt;
+//	}
+
+    public static HDT fromDataset(ExecutionEnvironment environment, String filePath) {
+        Preconditions.checkNotNull(filePath, "La ruta del archivo no puede ser nula...");
         HDT hdt = null;
 
         try {
-            hdt = HDTManager.generateHDT(filePath, "", RDFNotation.parse("ntriples"), new HDTSpecification(),null);
-
-//            HDT hdt1 = HDTManager.generateHDT(
-//                    rdfInput,         // Input RDF File
-//                    baseURI,          // Base URI
-//                    RDFNotation.parse(inputType), // Input Type
-//                    new HDTSpecification(),   // HDT Options
-//                    null              // Progress Listener
-
-
-
-
-        }catch (Exception e){
-
+            String baseURI = "file://" + System.getProperty("user.dir") + "/examples/dataset.nt";
+            hdt = HDTManager.generateHDT(filePath, baseURI, RDFNotation.parse("ntriples"), new HDTSpecification(), null);
+        } catch (Exception e) {
+            // Manejar la excepción (puedes imprimir un mensaje o registrar el error)
+            System.err.println("Error al generar el objeto HDT: " + e.getMessage());
+            e.printStackTrace();  // Esto imprimirá la traza de la excepción
         }
+
         return hdt;
-	}
+    }
+
+
 }
 
