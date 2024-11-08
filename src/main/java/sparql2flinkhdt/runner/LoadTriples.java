e6153e7a0f3fa5f18872d59a232fe90a2cb4a814
HenrryMatituy <henrrymatituy@gmail.com>
2024-11-08 15:02:46 -0500

    Revert "Test Carga de archivo"
    This reverts commit 65c5f9e728b913c27fd6efd202bd7d7445a13ffe.

--- src/main/java/sparql2flinkhdt/runner/LoadTriples.java
+++ src/main/java/sparql2flinkhdt/runner/LoadTriples.java
@@ -2,6 +2,7 @@ package sparql2flinkhdt.runner;
 
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.util.Preconditions;
+import org.apache.thrift.TSerializable;
 import org.rdfhdt.hdt.enums.RDFNotation;
 import org.rdfhdt.hdt.hdt.HDT;
 import org.rdfhdt.hdt.hdt.HDTManager;
@@ -13,39 +14,24 @@ import java.io.Serializable;
 
 public class LoadTriples implements Serializable {
     private static final Logger LOG = LoggerFactory.getLogger(LoadTriples.class);
-
     public static HDT fromDataset(ExecutionEnvironment environment, String filePath) {
         Preconditions.checkNotNull(filePath, "The file path may not be null...");
         HDT hdt = null;
 
         try {
-            // Log para verificar la ruta del archivo
-            LOG.info("Intentando generar HDT desde el archivo como antes: {}", filePath);
-
-            // Generar el HDT
-//          Flink 1.10.0
-//            hdt = HDTManager.generateHDT(filePath, "http://example.org/baseURI", RDFNotation.parse("ntriples"),new HDTSpecification(), null);
-//Flink 1.15.0 -->
-            //            hdt = HDTManager.generateHDT(filePath, "", RDFNotation.parse("ntriples"), new HDTSpecification(),null);
-
-//            Nueva versión
-            hdt = HDTManager.generateHDT(filePath, "", RDFNotation.parse("ntriples"),new HDTSpecification(), null);
-
-            // Log para verificar que la generación fue exitosa
-            if (hdt != null) {
-//                LOG.info("Generación de HDT exitosa. Número de triples: {}", hdt.getTriples().estimatedNumResults());
-                LOG.info("Número de sujetos únicos: {}", hdt.getDictionary().getNsubjects());
-                LOG.info("Número de predicados únicos: {}", hdt.getDictionary().getNpredicates());
-                LOG.info("Número de objetos únicos: {}", hdt.getDictionary().getNobjects());
-            } else {
-                LOG.error("La generación del HDT resultó en un objeto nulo.");
-            }
+            LOG.info("Intentando generar HDT desde el archivo: {}", filePath);
+            hdt = HDTManager.generateHDT(filePath, "http://example.org/baseURI", RDFNotation.parse("ntriples"), new HDTSpecification(), null);
+            LOG.info("Generación de HDT exitosa.");
 
         } catch (Exception e) {
-            // Log para informar cualquier excepción
             LOG.error("Error al generar HDT desde el archivo: {}", filePath, e);
         }
 
+        if (hdt == null) {
+            throw new RuntimeException("Error: no se pudo generar HDT desde el archivo: " + filePath);
+        }
         return hdt;
     }
+
 }
+
