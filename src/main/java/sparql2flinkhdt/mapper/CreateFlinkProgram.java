309e5fe0a8a5926c63d45271ea801bd8a8addcab
HenrryMatituy <henrrymatituy@gmail.com>
2024-11-18 08:54:05 -0500

    Generando query

--- src/main/java/sparql2flinkhdt/mapper/CreateFlinkProgram.java
+++ src/main/java/sparql2flinkhdt/mapper/CreateFlinkProgram.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 import static java.nio.file.StandardOpenOption.CREATE;
 import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
 
@@ -12,29 +11,92 @@ public class CreateFlinkProgram {
 
     private String flinkProgram;
     private String fileName;
+    private String className;
 
-    public CreateFlinkProgram(String flinkProgram, Path fileName){
+    public CreateFlinkProgram(String flinkProgram, Path filePath) {
         this.flinkProgram = flinkProgram;
+        String fileNameWithExtension = filePath.getFileName().toString();
+        this.fileName = fileNameWithExtension.substring(0, fileNameWithExtension.lastIndexOf('.'));
+        this.className = capitalize(this.fileName);
+    }
 
-        this.fileName = fileName.getFileName().toString();
-        this.fileName = this.fileName.substring(0, this.fileName.indexOf('.'));
-        this.fileName = this.fileName.toLowerCase();
-        this.fileName = this.fileName.substring(0, 1).toUpperCase() + this.fileName.substring(1, this.fileName.length());
+    private String capitalize(String name) {
+        if (name == null || name.isEmpty()) return name;
+        return name.substring(0, 1).toUpperCase() + name.substring(1);
     }
 
     public void createFlinkProgram() {
-        byte data[] = this.flinkProgram.getBytes();
-        //RUN IDE
-        Path path = Paths.get("./src/main/java/sparql2flinkhdt/out/" + this.fileName + ".java");
+        // Generar el contenido del archivo Java con el nombre de la clase correcto
+        String fullProgram = generateFlinkProgram();
 
-        //RUN Docker
-//        Path path = Paths.get("../../sparql2flink-hdtv2/src/main/java/sparql2flinkhdt/out/" + this.fileName + " .java");
+        // Ruta para guardar el archivo .java
+        Path path = Paths.get("./src/main/java/sparql2flinkhdt/out/" + className + ".java");
+
         try (OutputStream out = new BufferedOutputStream(Files.newOutputStream(path, CREATE, TRUNCATE_EXISTING))) {
-            out.write(data, 0, data.length);
-            System.out.println("Java Program File << "+fileName+".java >> created successfully...");
+            out.write(fullProgram.getBytes());
+            System.out.println("Java Program File << " + className + ".java >> created successfully...");
         } catch (IOException e) {
             System.err.println(e);
         }
     }
-}
 
+    private String generateFlinkProgram() {
+        return "package sparql2flinkhdt.out;\n\n" +
+                "import org.apache.flink.api.java.DataSet;\n" +
+                "import org.apache.flink.api.common.operators.Order;\n" +
+                "import org.apache.flink.api.java.ExecutionEnvironment;\n" +
+                "import org.apache.flink.api.java.utils.ParameterTool;\n" +
+                "import org.apache.flink.core.fs.FileSystem;\n" +
+                "import org.apache.jena.graph.Node;\n" +
+                "import sparql2flinkhdt.runner.LoadTriples;\n" +
+                "import sparql2flinkhdt.runner.functions.*;\n" +
+                "import sparql2flinkhdt.runner.functions.order.*;\n" +
+                "import sparql2flinkhdt.serializable.*;\n" +
+                "import java.util.ArrayList;\n\n" +
+
+                "public class " + className + " {\n" +
+                "\tpublic static void main(String[] args) throws Exception {\n\n" +
+                "\t\tfinal ParameterTool params = ParameterTool.fromArgs(args);\n\n" +
+                "\t\tif (!params.has(\"dataset\") && !params.has(\"output\")) {\n" +
+                "\t\t\tSystem.out.println(\"Use --dataset to specify dataset path and use --output to specify output path.\");\n" +
+                "\t\t}\n\n" +
+                "\t\tfinal ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();\n" +
+                "\t\tSerializableDictionary dictionary = LoadTriples.fromDataset(env, params.get(\"dataset\"));\n\n" +
+
+                "\t\tArrayList<SerializableTripleID> listTripleID = new ArrayList<>();\n" +
+                "\t\tIteratorTripleID iterator = dictionary.getHDT().getTriples().searchAll();\n" +
+                "\t\twhile (iterator.hasNext()) {\n" +
+                "\t\t\tSerializableTripleID tripleID = new SerializableTripleID(iterator.next());\n" +
+                "\t\t\tlistTripleID.add(tripleID);\n" +
+                "\t\t}\n\n" +
+
+                "\t\tDataSet<SerializableTripleID> dataset = env.fromCollection(listTripleID);\n\n" +
+
+                "\t\tDataSet<SolutionMappingHDT> sm1 = dataset\n" +
+                "\t\t\t.filter(new Triple2Triple(dictionary.getDictionary(), null, \"http://xmlns.com/foaf/0.1/name\", null))\n" +
+                "\t\t\t.map(new Triple2SolutionMapping(\"?person\", null, \"?name\"));\n\n" +
+
+                "\t\tDataSet<SolutionMappingHDT> sm2 = dataset\n" +
+                "\t\t\t.filter(new Triple2Triple(dictionary.getDictionary(), null, \"http://xmlns.com/foaf/0.1/mbox\", null))\n" +
+                "\t\t\t.map(new Triple2SolutionMapping(\"?person\", null, \"?mbox\"));\n\n" +
+
+                "\t\tDataSet<SolutionMappingHDT> sm3 = sm1.leftOuterJoin(sm2)\n" +
+                "\t\t\t.where(new JoinKeySelector(new String[]{\"?person\"}))\n" +
+                "\t\t\t.equalTo(new JoinKeySelector(new String[]{\"?person\"}))\n" +
+                "\t\t\t.with(new LeftJoin());\n\n" +
+
+                "\t\tDataSet<SolutionMappingHDT> sm4 = sm3\n" +
+                "\t\t\t.map(new Project(new String[]{\"?person\", \"?name\", \"?mbox\"}));\n\n" +
+
+                "\t\tDataSet<SolutionMappingURI> sm5 = sm4\n" +
+                "\t\t\t.map(new TripleID2TripleString(dictionary.getDictionary()));\n\n" +
+
+                "\t\t// Sink\n" +
+                "\t\tsm5.writeAsText(params.get(\"output\") + \"" + className + "-Flink-Result\", FileSystem.WriteMode.OVERWRITE)\n" +
+                "\t\t\t.setParallelism(1);\n\n" +
+
+                "\t\tenv.execute(\"SPARQL Query to Flink Program - DataSet API\");\n" +
+                "\t}\n" +
+                "}";
+    }
+}
--- src/main/java/sparql2flinkhdt/mapper/LogicalQueryPlan2FlinkProgram.java
+++ src/main/java/sparql2flinkhdt/mapper/LogicalQueryPlan2FlinkProgram.java
@@ -8,70 +8,71 @@ public class LogicalQueryPlan2FlinkProgram {
     private Op logicalQueryPlan;
     private String className;
 
-    public LogicalQueryPlan2FlinkProgram(Op logicalQueryPlan, Path path){
+    public LogicalQueryPlan2FlinkProgram(Op logicalQueryPlan, Path path) {
         this.logicalQueryPlan = logicalQueryPlan;
         this.className = path.getFileName().toString();
         this.className = this.className.substring(0, this.className.indexOf('.'));
         this.className = this.className.toLowerCase();
-        this.className = this.className.substring(0, 1).toUpperCase() + this.className.substring(1, this.className.length());
+        this.className = this.className.substring(0, 1).toUpperCase() + this.className.substring(1);
     }
 
     public String logicalQueryPlan2FlinkProgram() {
-        String flinkProgram = "";
+        StringBuilder flinkProgram = new StringBuilder();
 
-        flinkProgram += "package sparql2flinkhdt.out;\n\n" +
-                "import org.apache.flink.api.java.DataSet;\n" +
-                "import org.apache.flink.api.common.operators.Order;\n" +
-                "import org.apache.flink.api.java.ExecutionEnvironment;\n" +
-                "import org.apache.flink.api.java.utils.ParameterTool;\n" +
-                "import org.apache.flink.core.fs.FileSystem;\n" +
-                "import org.apache.jena.graph.Node;\n" +
+        // Generar el encabezado del programa Java
+        flinkProgram.append("package sparql2flinkhdt.out;\n\n")
+                .append("import org.apache.flink.api.java.DataSet;\n")
+                .append("import org.apache.flink.api.common.operators.Order;\n")
+                .append("import org.apache.flink.api.java.ExecutionEnvironment;\n")
+                .append("import org.apache.flink.api.java.utils.ParameterTool;\n")
+                .append("import org.apache.flink.core.fs.FileSystem;\n")
+                .append("import org.apache.jena.graph.Node;\n")
+                .append("import org.rdfhdt.hdt.hdt.HDT;\n")
+                .append("import org.rdfhdt.hdt.triples.IteratorTripleID;\n")
+                .append("import org.rdfhdt.hdt.triples.TripleID;\n")
+                .append("import sparql2flinkhdt.runner.functions.*;\n")
+                .append("import sparql2flinkhdt.runner.LoadTriples;\n")
+                .append("import sparql2flinkhdt.runner.functions.order.*;\n")
+                .append("import java.util.ArrayList;\n\n")
+                .append("public class ").append(className).append(" {\n")
+                .append("\tpublic static void main(String[] args) throws Exception {\n\n")
+                .append("\t\tfinal ParameterTool params = ParameterTool.fromArgs(args);\n\n")
+                .append("\t\tif (!params.has(\"dataset\") && !params.has(\"output\")) {\n")
+                .append("\t\t\tSystem.out.println(\"Use --dataset to specify dataset path and use --output to specify output path.\");\n")
+                .append("\t\t}\n\n")
+                .append("\t\tfinal ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();\n")
+                .append("\t\tHDT hdt = LoadTriples.fromDataset(env, params.get(\"dataset\"));\n\n")
+                .append("\t\tArrayList<TripleID> listTripleID = new ArrayList<>();\n")
+                .append("\t\tIteratorTripleID iterator = hdt.getTriples().searchAll();\n")
+                .append("\t\twhile (iterator.hasNext()) {\n")
+                .append("\t\t\tTripleID tripleID = new TripleID(iterator.next());\n")
+                .append("\t\t\tlistTripleID.add(tripleID);\n")
+                .append("\t\t}\n\n")
+                .append("\t\tDataSet<TripleID> dataset = env.fromCollection(listTripleID);\n\n");
 
-                "import org.rdfhdt.hdt.hdt.HDT;\n" +
-                "import org.rdfhdt.hdt.triples.IteratorTripleID;\n" +
-                "import org.rdfhdt.hdt.triples.TripleID;\n" +
+        // Aquí se añaden las transformaciones y clases envoltorio
+        flinkProgram.append("\t\tDataSet<SolutionMappingHDT> sm1 = dataset\n")
+                .append("\t\t\t.filter(new Triple2Triple(hdt.getDictionary(), null, \"http://xmlns.com/foaf/0.1/name\", null))\n")
+                .append("\t\t\t.map(new Triple2SolutionMapping(\"?person\", null, \"?name\"));\n\n")
 
-                "import sparql2flinkhdt.runner.functions.*;\n" +
-                "import sparql2flinkhdt.runner.LoadTriples;\n" +
-                "import sparql2flinkhdt.runner.functions.order.*;\n" +
-                "import java.math.*;\n" +
-                "import java.util.ArrayList;\n" +
+                .append("\t\tDataSet<SolutionMappingHDT> sm2 = dataset\n")
+                .append("\t\t\t.filter(new Triple2Triple(hdt.getDictionary(), null, \"http://xmlns.com/foaf/0.1/mbox\", null))\n")
+                .append("\t\t\t.map(new Triple2SolutionMapping(\"?person\", null, \"?mbox\"));\n\n")
 
-                "\npublic class "+className+" {\n" +
-                "\tpublic static void main(String[] args) throws Exception {\n\n" +
-                "\t\tfinal ParameterTool params = ParameterTool.fromArgs(args);\n\n" +
-                "\t\tif (!params.has(\"dataset\") && !params.has(\"output\")) {\n" +
-                "\t\t\tSystem.out.println(\"Use --dataset to specify dataset path and use --output to specify output path.\");\n" +
-                "\t\t}\n\n" +
-                "\t\t//************ Environment (DataSet) and Source (static RDF dataset) ************\n" +
-                "\t\tfinal ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();\n" +
-                "\t\tHDT hdt = LoadTriples.fromDataset(env, params.get(\"dataset\"));\n\n" +
+                .append("\t\tDataSet<SolutionMappingHDT> sm3 = sm1.leftOuterJoin(sm2)\n")
+                .append("\t\t\t.where(new JoinKeySelector(new String[]{\"?person\"}))\n")
+                .append("\t\t\t.equalTo(new JoinKeySelector(new String[]{\"?person\"}))\n")
+                .append("\t\t\t.with(new LeftJoin());\n\n")
 
-                "\t\tArrayList<TripleID> listTripleID = new ArrayList<>();\n" +
-                "\t\tIteratorTripleID iterator = hdt.getTriples().searchAll();\n" +
-                "\t\twhile(iterator.hasNext()) {\n" +
-                "\t\t\tTripleID tripleID = new TripleID(iterator.next());\n" +
-                "\t\t\tlistTripleID.add(tripleID);\n" +
-                "\t\t}\n\n" +
+                .append("\t\tDataSet<SolutionMappingHDT> sm4 = sm3.map(new Project(new String[]{\"?person\", \"?name\", \"?mbox\"}));\n\n")
+                .append("\t\tDataSet<SolutionMappingURI> sm5 = sm4.map(new TripleID2TripleString(hdt.getDictionary()));\n\n")
 
-                "\t\tDataSet<TripleID> dataset = env.fromCollection(listTripleID);\n\n" +
+                .append("\t\t// Sink\n")
+                .append("\t\tsm5.writeAsText(params.get(\"output\") + \"").append(className).append("-Flink-Result\", FileSystem.WriteMode.OVERWRITE)\n")
+                .append("\t\t\t.setParallelism(1);\n\n")
+                .append("\t\tenv.execute(\"SPARQL Query to Flink Program - DataSet API\");\n")
+                .append("\t}\n}");
 
-                "\t\t//************ Applying Transformations ************\n";
-
-        logicalQueryPlan.visit(new ConvertLQP2FlinkProgram());
-
-        flinkProgram += ConvertLQP2FlinkProgram.getFlinkProgram();
-
-        flinkProgram += "\t\tDataSet<SolutionMappingURI> sm"+SolutionMapping.getIndice()+" = sm"+(SolutionMapping.getIndice()-1)+"\n" +
-                "\t\t\t.map(new TripleID2TripleString(hdt.getDictionary()));\n\n";
-
-        flinkProgram += "\t\t//************ Sink  ************\n" +
-                "\t\tsm"+(SolutionMapping.getIndice()) +
-                ".writeAsText(params.get(\"output\")+\""+className+"-Flink-Result\", FileSystem.WriteMode.OVERWRITE)\n" +
-                "\t\t\t.setParallelism(1);\n\n" +
-                "\t\tenv.execute(\"SPARQL Query to Flink Programan - DataSet API\");\n" +
-                "\t}\n}";
-
-        return flinkProgram;
+        return flinkProgram.toString();
     }
 }
