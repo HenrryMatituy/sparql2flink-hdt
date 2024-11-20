2358f11ed4d4d7520f59d5990b71dddd93b1394d
HenrryMatituy <henrrymatituy@gmail.com>
2024-11-20 10:18:45 -0500

    Genera Query 1 pero no las otras

--- src/main/java/sparql2flinkhdt/out/Query.java
+++ src/main/java/sparql2flinkhdt/out/Query.java
@@ -6,34 +6,38 @@ import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.core.fs.FileSystem;
-import org.apache.jena.ext.xerces.impl.dv.xs.XSSimpleTypeDecl;
 import org.apache.jena.graph.Node_Literal;
-import org.rdfhdt.hdt.enums.TripleComponentRole;
+import org.apache.jena.ext.xerces.impl.dv.xs.XSSimpleTypeDecl;
 import org.rdfhdt.hdt.hdt.HDT;
 import org.rdfhdt.hdt.triples.IteratorTripleID;
 import org.rdfhdt.hdt.triples.TripleID;
 import sparql2flinkhdt.runner.SerializableDictionary;
 import sparql2flinkhdt.runner.LoadTriples;
 import sparql2flinkhdt.runner.functions.*;
-import sparql2flinkhdt.runner.functions.order.*;
+
 import java.util.ArrayList;
 
 public class Query {
 	public static void main(String[] args) throws Exception {
+
+		// Parsear los parámetros de entrada
 		final ParameterTool params = ParameterTool.fromArgs(args);
+
+		// Verificación de los parámetros necesarios
 		if (!params.has("dataset") || !params.has("output")) {
-			System.out.println("Use --dataset para especificar la ruta del dataset y --output para especificar la ruta de salida.");
+			System.out.println("Use --dataset y --output para especificar las rutas.");
 			return;
 		}
 
+		// Crear el entorno de ejecución de Flink
 		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 		env.getConfig().registerTypeWithKryoSerializer(Node_Literal.class, JavaSerializer.class);
 		env.getConfig().registerTypeWithKryoSerializer(XSSimpleTypeDecl.class, JavaSerializer.class);
 
+		// Cargar los triples del dataset HDT
 		HDT hdt = LoadTriples.fromDataset(env, params.get("dataset"));
-		SerializableDictionary serializableDictionary = new SerializableDictionary();
-		serializableDictionary.loadFromHDTDictionary(hdt.getDictionary());
 
+		// Convertir los triples de HDT a una lista de TripleID
 		ArrayList<TripleID> listTripleID = new ArrayList<>();
 		IteratorTripleID iterator = hdt.getTriples().searchAll();
 		while (iterator.hasNext()) {
@@ -41,8 +45,14 @@ public class Query {
 			listTripleID.add(tripleID);
 		}
 
+		// Crear un DataSet a partir de la lista de triples
 		DataSet<TripleID> dataset = env.fromCollection(listTripleID);
 
+		// Crear el diccionario serializable
+		SerializableDictionary serializableDictionary = new SerializableDictionary();
+		serializableDictionary.loadFromHDTDictionary(hdt.getDictionary());
+
+		// sm1: Primer filtro y mapeo
 		DataSet<SolutionMappingHDT> sm1 = dataset
 			.filter(new Triple2Triple(serializableDictionary, null, "http://xmlns.com/foaf/0.1/name", null))
 			.map(new MapFunction<TripleID, SolutionMappingHDT>() {
@@ -55,6 +65,7 @@ public class Query {
 				}
 			});
 
+		// sm2: Segundo filtro y mapeo
 		DataSet<SolutionMappingHDT> sm2 = dataset
 			.filter(new Triple2Triple(serializableDictionary, null, "http://xmlns.com/foaf/0.1/mbox", null))
 			.map(new MapFunction<TripleID, SolutionMappingHDT>() {
@@ -67,21 +78,25 @@ public class Query {
 				}
 			});
 
+		// Realizar un Left Outer Join entre sm1 y sm2
 		DataSet<SolutionMappingHDT> sm3 = sm1.leftOuterJoin(sm2)
 			.where(new JoinKeySelector(new String[]{"?person"}))
 			.equalTo(new JoinKeySelector(new String[]{"?person"}))
 			.with(new LeftJoin());
 
+		// Proyectar las variables ?person, ?name, y ?mbox
 		DataSet<SolutionMappingHDT> sm4 = sm3
 			.map(new Project(new String[]{"?person", "?name", "?mbox"}));
 
+		// Convertir los IDs a URIs usando el diccionario
 		DataSet<SolutionMappingURI> sm5 = sm4
 			.map(new TripleID2TripleString(serializableDictionary));
 
-		sm5.map(value -> value.toString())
-			.writeAsText(params.get("output") + "Query-Flink-Result", FileSystem.WriteMode.OVERWRITE)
+		//************ Sink ************
+		sm5.writeAsText(params.get("output") + "Query-Flink-Result", FileSystem.WriteMode.OVERWRITE)
 			.setParallelism(1);
 
+		// Ejecutar el job de Flink
 		env.execute("SPARQL Query to Flink Program - DataSet API");
 	}
 }
