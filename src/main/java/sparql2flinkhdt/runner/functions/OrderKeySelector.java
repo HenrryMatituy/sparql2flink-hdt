6fa1b76c7a568737de3cc263ce23d580b98c3df2
HenrryMatituy <henrrymatituy@gmail.com>
2025-02-07 19:31:55 -0500

    Casi funciona Query1

--- src/main/java/sparql2flinkhdt/runner/functions/OrderKeySelector.java
+++ src/main/java/sparql2flinkhdt/runner/functions/OrderKeySelector.java
@@ -1,29 +1,31 @@
 package sparql2flinkhdt.runner.functions;
 
 import org.apache.flink.api.java.functions.KeySelector;
-import org.apache.jena.graph.Node;
 import org.rdfhdt.hdt.dictionary.Dictionary;
+import org.rdfhdt.hdt.enums.TripleComponentRole;
+//import sparql2flinkhdt.runner.SolutionMappingHDT;
 
-// SolutionMapping - Key Selector Order by
 public class OrderKeySelector implements KeySelector<SolutionMappingHDT, String> {
 
-    private static Dictionary dictionary = null;
-	private String key;
+	private Dictionary dictionary;  // Diccionario HDT para convertir IDs a strings
+	private String key;  // La clave (variable SPARQL) por la cual se ordenará
 
 	public OrderKeySelector(Dictionary dictionary, String key) {
-	    this.dictionary = dictionary;
+		this.dictionary = dictionary;
 		this.key = key;
 	}
 
 	@Override
 	public String getKey(SolutionMappingHDT sm) {
-        String value = "";
-//	    Node node = TripleIDConvert.idToString(dictionary, sm.getMapping().get(key));
-//        if(node.isLiteral()) {
-//            value = node.getLiteralValue().toString();
-//        }else if(node.isURI()){
-//            value = sm.getMapping().get(key).toString();
-//        }
-        return value;
+		// Obtener el valor asociado a la clave (por ejemplo, "?label")
+		SolutionMappingHDT.MappingValue mappingValue = sm.getMapping().get(key);
+
+		if (mappingValue != null) {
+			// Convertir el valor a una cadena utilizando el diccionario HDT
+			return dictionary.idToString(mappingValue.getId(), mappingValue.getRole()).toString();
+		} else {
+			// Si no hay valor asociado, devolver una cadena vacía
+			return "";
+		}
 	}
 }
